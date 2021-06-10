7cf04903c080c076257896da0fbe10934ec17cc9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 17:49:51 +0530

    add css to greeting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-let curDate = new Date(2021,5,5,20);
+let curDate = new Date(2021,5,5,8);
 curDate = curDate.getHours();
 let greeting ="";
-
+const cssGreeting={ };
 
 if(curDate>=1 && curDate<12){
     greeting="Good Morning";
+    cssGreeting.color='green';
 }
 else if(curDate>=12 && curDate<=19){
     greeting="Good AfterNoon";
+    cssGreeting.color='red';
+
 }
 else{
     greeting="Good Night"
+    cssGreeting.color='black';
+
 }
 
 ReactDom.render(
-    <h1>Hello sir, {greeting}</h1>
+    <h1>Hello sir, <span style={cssGreeting}>{greeting}</span></h1>
     ,document.getElementById('root')
 );
