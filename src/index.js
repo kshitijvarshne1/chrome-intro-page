7c1f980642653edf8fb7ad164faccc376552352a
kshitij varshney <kshitijvarshney92@gmail.com>
2021-06-10 17:47:46 +0530

    add code for greeting format and display

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render(
+let curDate = new Date(2021,5,5,20);
+curDate = curDate.getHours();
+let greeting ="";
 
+
+if(curDate>=1 && curDate<12){
+    greeting="Good Morning";
+}
+else if(curDate>=12 && curDate<=19){
+    greeting="Good AfterNoon";
+}
+else{
+    greeting="Good Night"
+}
+
+ReactDom.render(
+    <h1>Hello sir, {greeting}</h1>
+    ,document.getElementById('root')
 );
